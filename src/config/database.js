e5c0ad86a1aa52142d1ec1ff4a0d5686efd0f775
Nayan Thapa Magar <thapamagarnayan393@gamail.com>
2022-09-30 12:24:56 +0545

    refactor: best paractice lint

--- src/config/database.js
+++ src/config/database.js
@@ -5,11 +5,12 @@ require('dotenv').config();
 module.exports = async (req, res) => {
   // URI for connection to  mongobd databasae
   const dbURI = process.env.DB_URI;
-  // wrapping(putting/ assigning) the database connection in a variable to establish batabase connecton at required places
+  // wrapping the database connection in a variable
+  // to establish batabase connecton at required places
   const mongo = await mongoose
     .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => console.log('Connected to database'))
-    .catch((err) => res.send({
+    .then(() => console.log('Connected to database'))
+    .catch(() => res.send({
       databaseConnection: false,
       message: 'Failed to connect to database',
     }));
