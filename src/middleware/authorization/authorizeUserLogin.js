ec79713c2746f36e57a8a303f5cf5a8cf68c68fb
Nayan Thapa Magar <thapamagarnayan393@gamail.com>
2022-10-04 16:54:16 +0545

    refactor: authHeader removed

--- src/middleware/authorization/authorizeUserLogin.js
+++ src/middleware/authorization/authorizeUserLogin.js
@@ -6,9 +6,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   // getting access token from headers
   const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
-  //   const token = authHeader && authHeader.split(' ')[1];
-
+  const token = authHeader.split(' ')[1];
   // checking if access token is null
   if (token == null) return res.send({ requestAccess: false, message: 'Access Denied' });
   // verifying access token
