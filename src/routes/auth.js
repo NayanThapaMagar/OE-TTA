1315ca8af1817b3f57472c79836c6db110791b09
Nayan Thapa Magar <thapamagarnayan393@gamail.com>
2022-10-04 16:52:59 +0545

    refactor: add user route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,24 +4,24 @@ const router = express.Router();
 
 // importing controller
 const login = require('../controller/auth/login');
-const register = require('../controller/auth/register/company');
-// const addUser = require("../controller/auth/addUser");
+const registerCompany = require('../controller/auth/register/company');
+const registerUser = require('../controller/auth/register/user');
 // const logout = require("../controller/auth/logout");
 // const refreshAccessTokenSecret = require("../controller/auth/refreshAccessTokenSecret");
 
 // requiring authorization middleware
-// const authorizeUserLogin = require("../middleware/authorization/authorizeUserLogin");
+const authorizeUserLogin = require('../middleware/authorization/authorizeUserLogin');
 // const authorizeRootUser = require("../middleware/authorization/authorizeRootUser")
 
 // handeling login request
 router.post('/login', login);
 // handeling registering request
-router.post('/register', register);
+router.post('/register', registerCompany);
 // // handeling logout request
 // router.post("/logout", authorizeUserLogin, logout);
 // // handeling refresh access token request
 // router.get("/refreshtoken", authorizeUserLogin, refreshAccessTokenSecret);
 // // handeling request to add users of two different level
-// router.post("/addUser", authorizeUserLogin, authorizeRootUser, addUser);
+router.post('/addUser', authorizeUserLogin, registerUser);
 
 module.exports = router;
